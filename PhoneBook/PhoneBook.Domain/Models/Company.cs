7b61e5a3b201ff58310df015395345fb672af5d6
MatthewCaruana <matthew.caruana99@gmail.com>
2023-12-02 15:56:26 +0100

    Adding Domain Models

---
+++ PhoneBook/PhoneBook.Domain/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Domain.Models
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public Company(string companyName, DateTime registrationDate)
+        {
+            CompanyName = companyName;
+            RegistrationDate = registrationDate;
+        }
+    }
+}
