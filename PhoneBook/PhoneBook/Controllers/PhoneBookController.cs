cb3348042eed1eb9fb0200efe3be2e4a87fb0805
MatthewCaruana <matthew.caruana99@gmail.com>
2023-12-02 15:57:07 +0100

    Adding PhoneBookController

---
+++ PhoneBook/PhoneBook/Controllers/PhoneBookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Application.DTOs;
+using PhoneBook.Application.Services.Interfaces;
+
+namespace PhoneBook.Controllers
+{
+    [ApiController]
+    [Route("PhoneBook")]
+    public class PhoneBookController : ControllerBase
+    {
+        private readonly ILogger<PhoneBookController> _logger;
+        private ICompanyServices _companyServices;
+
+        public PhoneBookController(ILogger<PhoneBookController> logger, ICompanyServices companyServices)
+        {
+            _logger = logger;
+            _companyServices = companyServices;
+        }
+
+        [HttpGet(Name ="GetAllCompanies")]
+        public List<CompanyDTO> GetAllCompanies()
+        {
+            List<CompanyDTO> result = _companyServices.GetAllCompanies();
+            return result;
+        }
+    }
+}
--- PhoneBook/PhoneBook/Program.cs
+++ PhoneBook/PhoneBook/Program.cs
@@ -1,3 +1,9 @@
+using PhoneBook.Application.Services;
+using PhoneBook.Application.Services.Interfaces;
+using PhoneBook.Persistence.Context;
+using PhoneBook.Persistence.Repositories;
+using PhoneBook.Persistence.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PhoneBookContext>();
+
+builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
+builder.Services.AddTransient<IPersonRepository, PersonRepository>();
+
+builder.Services.AddScoped<ICompanyServices, CompanyServices>();
 
 var app = builder.Build();
 
